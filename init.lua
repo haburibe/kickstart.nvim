89770190cc73eeb35377045898182fbda62b57f6
haburibe <haburibe@gmail.com>
2024-09-07 19:14:50 +0900

    show tabline always

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Show tabline always
+vim.opt.showtabline = 2
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
