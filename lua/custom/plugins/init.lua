8237024f407f9e75cec4f7088ecfad1713e17b3e
haburibe <haburibe@gmail.com>
2024-09-07 17:26:05 +0900

    use nvim-surround

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,16 @@
 -- See the kickstart.nvim README for more information
 return {
   {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    opts = {},
+  },
+  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     opts = {},
